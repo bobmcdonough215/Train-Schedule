93eeda89880d13c8edbdbd18c4d3f8d3c060c1f6
Bob McDonough <bobmcdonough215@gmail.com>
2019-07-14 12:37:51 -0400

    initialized firebase

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+// 1. Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD6aVi7cxftC9TqQRXec26yMb9YR-paCp4",
+    authDomain: "bobs-test-project-41b0b.firebaseapp.com",
+    databaseURL: "https://bobs-test-project-41b0b.firebaseio.com",
+    projectId: "bobs-test-project-41b0b",
+    storageBucket: "bobs-test-project-41b0b.appspot.com",
+    messagingSenderId: "183492627215",
+    appId: "1:183492627215:web:2993a5e21921290a"
+  };
+  // Initialize Firebase  
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
